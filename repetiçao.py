6fb14738907c302a3a8209b31dbf3e130154fdac
FernandaSoubhia <soubhiafernanda@gmail.com>
2026-06-01 12:59:10 -0300

    Add files via upload

--- repetiçao.py
+++ repetiçao.py
@@ -1,10 +1,16 @@
-i=1  #contador - inicio
-while(i<=20):
-    print(i)
-    i=i+1    #incremento do contador 
+#i=1  #contador - inicio
+#while(i<=20):
+#    print(i)
+#    i=i+1    #incremento do contador 
 
-i=20
-while(i>=1):
-    print(i)
-    i=i-1   
+#i=20
+#while(i>=1):
+#    print(i)
+#    i=i-1   
 
+opcao='S'
+
+while(opcao=='S'):
+    opcao=input('Deseja continuar (S/N): ')
+
+print('Encerrado programa...')
