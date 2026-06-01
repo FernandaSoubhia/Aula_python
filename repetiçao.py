0da44c256ea0e2e3948c566434057fc5fca4df36
FernandaSoubhia <soubhiafernanda@gmail.com>
2026-06-01 13:10:23 -0300

    Add files via upload

--- repetiçao.py
+++ repetiçao.py
@@ -8,9 +8,22 @@
 #    print(i)
 #    i=i-1   
 
-opcao='S'
+#opcao='S'
 
-while(opcao=='S'):
-    opcao=input('Deseja continuar (S/N): ')
+#while(opcao=='S'):
+#    opcao=input('Deseja continuar (S/N): ')
 
-print('Encerrado programa...')
+#print('Encerrado programa...')
+
+#for i in range(1, 20): # este para um numero antes do q vc quer ou vc coloca 20+1 para q ele montre o numero desejavel
+ #   print(i)
+
+#inicio=int(input('Valor Inicial':)
+#fim=int(input('VAlor Final: '))
+#for i in range (inicio, fim+1)
+#    print(i)
+
+inicio=int(input('Valor Inicial: '))
+fim=int(input('VAlor Final: '))
+for i in range (inicio, fim+1,2):    #inicio, fim+1,2 se vc coloca a , depois de add um +numero vc quer q ele pule este numero
+    print(i)
